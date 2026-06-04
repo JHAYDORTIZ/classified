b3db26b1aabac761c537612807ea3b81fc0de7e5
JHAYDORTIZ <victorjesusortizdeniz17@gmail.com>
2026-06-04 02:44:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,38 @@
 const PASSWORD = "4tanyatapes";
 
-/* LOGIN */
+const loginBox = document.getElementById("login");
+const desktop = document.getElementById("desktop");
+const error = document.getElementById("error");
+
+document
+  .getElementById("enterBtn")
+  .addEventListener("click", login);
+
 function login() {
-  const input = document.getElementById("password").value;
-  const error = document.getElementById("error");
 
-  if (input === PASSWORD) {
-    document.getElementById("login").classList.add("hidden");
-    document.getElementById("desktop").classList.remove("hidden");
-  } else {
+  const value =
+    document.getElementById("password").value;
+
+  if(value === PASSWORD){
+
+    loginBox.style.display = "none";
+    desktop.style.display = "block";
+
+  }else{
+
     error.textContent = "ACCESS DENIED";
+
   }
 }
 
-/* WINDOWS */
-function openWindow(id) {
-  document.getElementById(id).classList.remove("hidden");
+function openWindow(id){
+  document
+    .getElementById(id)
+    .classList.remove("hidden");
 }
 
-function closeWindow(id) {
-  document.getElementById(id).classList.add("hidden");
+function closeWindow(id){
+  document
+    .getElementById(id)
+    .classList.add("hidden");
 }
-
-/* DRAG SYSTEM */
-let dragItem = null;
-let offsetX = 0;
-let offsetY = 0;
-
-document.addEventListener("mousedown", (e) => {
-  if (e.target.classList.contains("titlebar")) {
-    dragItem = e.target.parentElement;
-    offsetX = e.offsetX;
-    offsetY = e.offsetY;
-  }
-});
-
-document.addEventListener("mousemove", (e) => {
-  if (dragItem) {
-    dragItem.style.left = (e.pageX - offsetX) + "px";
-    dragItem.style.top = (e.pageY - offsetY) + "px";
-  }
-});
-
-document.addEventListener("mouseup", () => {
-  dragItem = null;
-});
