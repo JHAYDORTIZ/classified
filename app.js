0018a9eed67a5654684e5af2ee9514103e5b57a3
JHAYDORTIZ <victorjesusortizdeniz17@gmail.com>
2026-06-04 22:48:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,9 +22,10 @@ function renderFolder(folderName) {
   const windowEl = document.getElementById(folderName);
   const content = windowEl.querySelector(".content");
 
- const files = fileSystem.filter(f =>
-  f.split("/")[1] === folderName
-);
+  const files = fileSystem.filter(f => {
+    const parts = f.split("/");
+    return parts.length >= 3 && parts[1] === folderName;
+  });
 
   content.innerHTML = files.length
     ? files.map(f => `<div>📄 ${f.split("/").pop()}</div>`).join("")
