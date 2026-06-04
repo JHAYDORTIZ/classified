25c31563d59c85f2e4e352d5c4e3242de43f16eb
JHAYDORTIZ <victorjesusortizdeniz17@gmail.com>
2026-06-04 03:01:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,21 +3,18 @@ const PASSWORD = "4tanyatapes";
 /* LOGIN */
 function login() {
   const value = document.getElementById("password").value;
-  const error = document.getElementById("error");
 
   if (value === PASSWORD) {
-    document.getElementById("login").style.display = "none";
-    document.getElementById("desktop").style.display = "block";
+    document.getElementById("login").classList.add("hidden");
+    document.getElementById("desktop").classList.remove("hidden");
   } else {
-    error.textContent = "ACCESS DENIED";
+    document.getElementById("error").textContent = "ACCESS DENIED";
   }
 }
 
 /* WINDOWS */
 function openWindow(id) {
-  const win = document.getElementById(id);
-  win.style.display = "block";
-  win.style.zIndex = 999;
+  document.getElementById(id).style.display = "block";
 }
 
 function closeWindow(id) {
