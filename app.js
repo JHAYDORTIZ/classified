0dace7dfb2b8a4c253a97db96c123c0b2ac1aa59
JHAYDORTIZ <victorjesusortizdeniz17@gmail.com>
2026-06-05 02:34:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -211,8 +211,6 @@ function setTime(v){
   audio.currentTime = (v / 100) * audio.duration;
 }
 
-content = `<img src="${path}" style="max-width:100%; max-height:100%;">`;
-
 /* DRAG WINDOWS */
 let dragWindow = null;
 let offsetX = 0;
