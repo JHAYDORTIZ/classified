dd96a35fa441d4f48b191ee7d42b1b14b87ef940
JHAYDORTIZ <victorjesusortizdeniz17@gmail.com>
2026-06-05 02:09:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -149,6 +149,12 @@ function openFromSearch(name){
 let tracks = [];
 let current = 0;
 let audio = new Audio();
+audio.ontimeupdate = () => {
+  const bar = document.getElementById("seekBar");
+  if (!audio.duration) return;
+
+  bar.value = (audio.currentTime / audio.duration) * 100;
+};
 
 fetch("index-media.json")
   .then(r => r.json())
